35a96bcefedc9e7a54c825e36c0a06db952b31f4
Shankar Murali <18612515+shkgiri@users.noreply.github.com>
2024-12-26 19:53:50 +0000

    yy

--- knowledge_graph.py
+++ knowledge_graph.py
@@ -8,62 +8,63 @@ hv.extension('bokeh')
 
 
 def create_expandable_graph():
-  # Initial graph with base nodes
-  G = nx.Graph()
+
+   # Initial graph with base nodes
+    G = nx.Graph()
 
   # Add initial nodes and edges
   # Start with just A and B visible
-  G.add_node('A', visible=True)
-  G.add_node('B', visible=True)
-  G.add_edge('A', 'B', relation='uses', visible=True)
+    G.add_node('A', visible=True)
+    G.add_node('B', visible=True)
+    G.add_edge('A', 'B', relation='uses', visible=True)
 
   # Add hidden nodes and edges
-  G.add_node('D', visible=False)
-  G.add_node('1', visible=False)
-  G.add_node('2', visible=False)
-  G.add_edge('B', 'D', relation='detects', visible=False)
-  G.add_edge('D', '1', relation='mitigates', visible=False)
-  G.add_edge('D', '2', relation='mitigates', visible=False)
+    G.add_node('D', visible=False)
+    G.add_node('1', visible=False)
+    G.add_node('2', visible=False)
+    G.add_edge('B', 'D', relation='detects', visible=False)
+    G.add_edge('D', '1', relation='mitigates', visible=False)
+    G.add_edge('D', '2', relation='mitigates', visible=False)
 
   # Handle right-click events using selection
-  def handle_selection(event):
-      if event.element and event.element.node_renderer.data['name'] == 'B':
-          G.nodes['D']['visible'] = not G.nodes['D']['visible']
-          G['B']['D']['visible'] = not G['B']['D']['visible']
-      elif event.element and event.element.node_renderer.data['name'] == 'D':
-          G.nodes['1']['visible'] = not G.nodes['1']['visible']
-          G.nodes['2']['visible'] = not G.nodes['2']['visible']
-          G['D']['1']['visible'] = not G['D']['1']['visible']
-          G['D']['2']['visible'] = not G['D']['2']['visible']
-      graph.source = G  # Update graph source with modified visibility
+    def handle_selection(event):
+        if event.element and event.element.node_renderer.data['name'] == 'B':
+            G.nodes['D']['visible'] = not G.nodes['D']['visible']
+            G['B']['D']['visible'] = not G['B']['D']['visible']
+        elif event.element and event.element.node_renderer.data['name'] == 'D':
+            G.nodes['1']['visible'] = not G.nodes['1']['visible']
+            G.nodes['2']['visible'] = not G.nodes['2']['visible']
+            G['D']['1']['visible'] = not G['D']['1']['visible']
+            G['D']['2']['visible'] = not G['D']['2']['visible']
+        graph.source = G  # Update graph source with modified visibility
 
-  # Create a dynamic graph that updates on selection
-  graph = hv.DynamicMap(
-      lambda graph: hv.Graph.from_networkx(graph, nx.spring_layout),
-      streams=[hv.streams.PointerXY(source=graph)]  # Use the defined graph here
-  )
+    # Create a dynamic graph that updates on selection
+    graph = hv.DynamicMap(
+        lambda graph: hv.Graph.from_networkx(graph, nx.spring_layout),
+        streams=[hv.streams.PointerXY(source=graph)]  # Use the defined graph here
+    )
 
-  # Style the graph
-  graph.opts(
-      opts.Graph(
-          tools=['tap', 'box_select'],
-          node_size=30,
-          node_color='blue',
-          edge_color='gray',
-          width=800,
-          height=400,
-          xaxis=None,
-          yaxis=None,
-          node_hover_fill_color='red',
-          edge_hover_line_color='red',
-          edge_hover_line_width=3
-      )
-  )
+    # Style the graph
+    graph.opts(
+        opts.Graph(
+            tools=['tap', 'box_select'],
+            node_size=30,
+            node_color='blue',
+            edge_color='gray',
+            width=800,
+            height=400,
+            xaxis=None,
+            yaxis=None,
+            node_hover_fill_color='red',
+            edge_hover_line_color='red',
+            edge_hover_line_width=3
+        )
+    )
 
-  # Connect selection events to handle_selection function
-  graph.events('selection').subscribe(handle_selection)
+    # Connect selection events to handle_selection function
+    graph.events('selection').subscribe(handle_selection)
 
-  return graph
+    return graph
 
 
 # Create and display the graph
